f345d7e0deb8f33d652d0da3b0027fd330ef12d6
Николай Кокорев <gruffbarbecue@gmail.com>
2020-05-01 23:08:17 +0300

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,6 +5,5 @@ from car import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.car_list),
-    path('index/', views.index),
-    path('main/', views.CarsView),
+    path('index/', CarsView.as_view(), name='main'),
 ]
